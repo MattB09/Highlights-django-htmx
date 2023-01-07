5d9751842b9fd4c23a19292a00a9e3eac22f20e6
mattb09 <mattbancroft03@gmail.com>
2023-01-07 16:54:04 +0900

    Added querysets to models

--- highlights/models.py
+++ highlights/models.py
@@ -9,7 +9,18 @@ from django.urls import reverse
 from .utils import slugify
 
 
+class BaseQuerySet(models.QuerySet):
+    def for_user(self, user):
+        return self.filter(Q(user=user))
+    
+    
+class AuthorQuerySet(BaseQuerySet):
+    pass
+    
+    
 class Author(models.Model):
+    objects = AuthorQuerySet.as_manager()
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(null=True, blank=True)
@@ -36,7 +47,12 @@ def author_pre_save(sender, instance, *args, **kwargs):
 pre_save.connect(author_pre_save, sender=Author)
 
 
+class BookQuerySet(BaseQuerySet):
+    pass
+
 class Book(models.Model):
+    objects = BookQuerySet.as_manager()
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(null=True, blank=True)
@@ -68,12 +84,16 @@ def book_pre_save(sender, instance, *args, **kwargs):
 pre_save.connect(book_pre_save, sender=Book)
 
 
-class HighlightQuerySet(models.QuerySet):
-    def search(self, query=None):
+class HighlightQuerySet(BaseQuerySet):
+    def search(self, user, query=None):
+        qs = self.for_user(user)
         if query is None or query == "":
-            return self.all()
+            return qs
         lookups = Q(text__icontains=query)
-        return self.filter(lookups)
+        return qs.filter(lookups)
+    
+    def find_duplicates(self, user, text, id=None):
+        return self.for_user(user).filter(Q(text=text)).exclude(id=id)
 
 
 class Highlight(models.Model):
@@ -107,17 +127,30 @@ class Highlight(models.Model):
         return highlight
 
     def update(self, text, book, tags):
-        print("BOOK", book)
-        print("BOOK", type(book))
         self.text = text
         self.book = book
         self.tags.set(tags)
         self.save(update_fields=["text", "book"])
 
 
+class TagQuerySet(BaseQuerySet):
+    def search(self, user, query=None):
+        qs = self.for_user(user)
+        if query is None or query == "":
+            return qs.all()
+        lookups = Q(tag__icontains=query)
+        return qs.filter(lookups)
+    
+
 class Tag(models.Model):
+    objects = TagQuerySet.as_manager()
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    tag = models.CharField(max_length=255, db_index=True)
+    tag = models.CharField(
+        max_length=255, 
+        db_index=True, 
+        error_messages={"unique": "This tag already exists."},
+    )
     slug = models.SlugField(null=True, blank=True)
     user = models.ForeignKey(User, related_name="tags", on_delete=models.CASCADE)
     highlights = models.ManyToManyField(Highlight, related_name="tags", blank=True,)
@@ -131,6 +164,15 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag
+    
+    @classmethod
+    def new(cls, tag, user):
+        tag = cls.objects.create(tag=tag, user=user)
+        return tag
+        
+    def update(self, tag):
+        self.tag = tag
+        self.save(update_fields=["tag"])
 
 
 def tag_pre_save(sender, instance, *args, **kwargs):
